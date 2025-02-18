b916c3860a8bad086a1d32d9bd117112cc3e48c3
JonatasRodriguesReis <jonatas.rodriguesreis@gmail.com>
2025-02-18 18:07:00 -0400

    Fix script to upload email templates

--- scripts/upload-mailgun-templates.ts
+++ scripts/upload-mailgun-templates.ts
@@ -8,26 +8,18 @@ import FormData from "form-data";
 const MAILGUN_API_KEY = process.env.MAILGUN_API_KEY;
 const MAILGUN_DOMAIN = process.env.MAILGUN_DOMAIN;
 const MAILGUN_USERNAME = process.env.MAILGUN_USERNAME ?? "api";
+const LATEST_TAG = "latest";
 
-const uploadTemplate = async (
-  templateName: string,
-  templateContent: string
-) => {
-  const url = `https://api.mailgun.net/v3/${MAILGUN_DOMAIN}/templates`;
+async function getTemplate(templateName: string) {
+  const url = `https://api.mailgun.net/v3/${MAILGUN_DOMAIN}/templates/${templateName}/versions`;
 
   try {
     if (!MAILGUN_API_KEY) {
       throw new Error("MAILGUN_API_KEY is not set");
     }
 
-    const form = new FormData();
-    form.append("name", templateName);
-    form.append("description", `Template for ${templateName}`);
-    form.append("template", templateContent);
-
-    await axios.post(url, form, {
+    const response = await axios.get(url, {
       headers: {
-        ...form.getHeaders(),
         Authorization:
           "Basic " +
           Buffer.from(`${MAILGUN_USERNAME}:${MAILGUN_API_KEY}`).toString(
@@ -35,7 +27,96 @@ const uploadTemplate = async (
           ),
       },
     });
-    console.log(`Uploaded template: ${templateName}`);
+    if (response.status === 200 && response.data.template) {
+      const template = response.data.template;
+      const templateName = template.name;
+      const versions: { tag: string; active: boolean }[] = template.versions;
+      return { templateName, versions };
+    }
+  } catch (error) {
+    console.log(`Template not found: ${templateName}`);
+    return undefined;
+  }
+}
+
+const uploadTemplate = async (
+  templateName: string,
+  templateContent: string,
+  existingTemplates:
+    | { templateName: string; versions: { tag: string; active: boolean }[] }
+    | undefined
+) => {
+  const url = `https://api.mailgun.net/v3/${MAILGUN_DOMAIN}/templates`;
+
+  try {
+    if (!MAILGUN_API_KEY) {
+      throw new Error("MAILGUN_API_KEY is not set");
+    }
+    const templateExistsAndHasLatestVersion =
+      existingTemplates &&
+      existingTemplates.versions.some((version) => version.tag === LATEST_TAG);
+    const templateExistsWithoutLatestVersion =
+      existingTemplates && !templateExistsAndHasLatestVersion;
+
+    //Update the template if it exists and has the latest version
+    if (templateExistsAndHasLatestVersion) {
+      console.log(
+        `Template ${templateName} already exists with latest version`
+      );
+      const form = new FormData();
+      form.append("template", templateContent);
+      form.append("active", "true");
+      await axios.put(`${url}/${templateName}/versions/${LATEST_TAG}`, form, {
+        headers: {
+          ...form.getHeaders(),
+          Authorization:
+            "Basic " +
+            Buffer.from(`${MAILGUN_USERNAME}:${MAILGUN_API_KEY}`).toString(
+              "base64"
+            ),
+        },
+      });
+    }
+    // Update the template by creating the latest version
+    else if (templateExistsWithoutLatestVersion) {
+      console.log(
+        `Template ${templateName} already exists but without latest version`
+      );
+      const form = new FormData();
+      form.append("tag", LATEST_TAG);
+      form.append("template", templateContent);
+      form.append("active", "true");
+      await axios.post(`${url}/${templateName}/versions`, form, {
+        headers: {
+          ...form.getHeaders(),
+          Authorization:
+            "Basic " +
+            Buffer.from(`${MAILGUN_USERNAME}:${MAILGUN_API_KEY}`).toString(
+              "base64"
+            ),
+        },
+      });
+    }
+    // Create a new template and the latest version
+    else {
+      const form = new FormData();
+      form.append("name", templateName);
+      form.append("description", `Template for ${templateName}`);
+      form.append("template", templateContent);
+      form.append("tag", LATEST_TAG);
+
+      await axios.post(url, form, {
+        headers: {
+          ...form.getHeaders(),
+          Authorization:
+            "Basic " +
+            Buffer.from(`${MAILGUN_USERNAME}:${MAILGUN_API_KEY}`).toString(
+              "base64"
+            ),
+        },
+      });
+      console.log(`Uploaded template: ${templateName}`);
+    }
   } catch (error) {
     console.error(`Failed to upload template: ${templateName}`, error);
   }
@@ -71,8 +152,9 @@ const uploadTemplates = async () => {
       path.join(templatesDir, file),
       "utf-8"
     );
+    const existingTemplate = await getTemplate(templateName);
     const processedContent = preprocessTemplate(templateContent, partialsDir);
-    await uploadTemplate(templateName, processedContent);
+    await uploadTemplate(templateName, processedContent, existingTemplate);
   }
 };
 
